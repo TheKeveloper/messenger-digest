8b3952bb24c47e7e62216a8e48a48a1bf6913065
Kevin Bi <keveloper@gmail.com>
2023-05-29 16:49:19 -0400

    Enable setting open ai configuration

--- src/options.tsx
+++ src/options.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { OpenAiConfiguration } from "./api/OpenAiConfiguration";
-import { loadOpenAiConfiguration } from "./storage/storage";
+import { loadOpenAiConfiguration, saveOpenAiConfiguration } from "./storage/storage";
 import { LoadingState } from "./api/loading";
+import { trimToUndefined } from "./utils/stringUtils";
+import { Button, Container, TextField } from "@mui/material";
 
 const Options = () => {
   const [openAiConfig, setOpenAiConfig] = useState<LoadingState<OpenAiConfiguration>>(LoadingState.loading());
-  const [color, setColor] = useState<string>("");
-  const [status, setStatus] = useState<string>("");
-  const [like, setLike] = useState<boolean>(false);
 
   useEffect(() => {
     loadOpenAiConfiguration().then(LoadingState.loaded).catch(error => {
@@ -17,68 +16,44 @@ const Options = () => {
     }).then(setOpenAiConfig)
   }, []);
 
-  useEffect(() => {
-    // Restores select box and checkbox state using the preferences
-    // stored in chrome.storage.
-    chrome.storage.sync.get(
-      {
-        favoriteColor: "red",
-        likesColor: true,
-      },
-      (items) => {
-        setColor(items.favoriteColor);
-        setLike(items.likesColor);
-      }
-    );
-  }, []);
+  const onSave = useCallback(async () => {
+    const curConfig = LoadingState.getLoaded(openAiConfig) ?? {};
+    try {
+      console.info("Before save");
+      await saveOpenAiConfiguration(curConfig);
+      console.info("Saved: ", curConfig);
+    } catch (e) {
+      console.warn("Failed to save to local storage", e);
+    }
+  }, [openAiConfig]);
 
-  const saveOptions = () => {
-    // Saves options to chrome.storage.sync.
-    chrome.storage.sync.set(
-      {
-        favoriteColor: color,
-        likesColor: like,
-      },
-      () => {
-        // Update status to let user know options were saved.
-        setStatus("Options saved.");
-        const id = setTimeout(() => {
-          setStatus("");
-        }, 1000);
-        return () => clearTimeout(id);
-      }
-    );
-  };
+  const onApiKeyChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const prevConfig = LoadingState.getLoaded(openAiConfig) ?? {};
+    setOpenAiConfig(LoadingState.loaded({...prevConfig, apiKey: trimToUndefined(event.target.value)}));
+  }, [openAiConfig, setOpenAiConfig]);
+
+  const onOrganizationIdChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const prevConfig = LoadingState.getLoaded(openAiConfig) ?? {};
+    setOpenAiConfig(LoadingState.loaded({...prevConfig, organizationId: trimToUndefined(event.target.value)}));
+  }, [openAiConfig, setOpenAiConfig]);
+
+  if (openAiConfig.status === "loading" || openAiConfig.status === "reloading") {
+    return <div>Loading...</div>;
+  }
+
+  if (openAiConfig.status === "failed") {
+    return <div>Failed to load data from chrome storage</div>;
+  }
+
+
 
   return (
-    <React.Fragment>
-      <div>
-        API Key: { LoadingState.getLoaded(openAiConfig)?.apiKey ?? "Not found"}
-      </div>
-      <div>
-        Favorite color: <select
-          value={color}
-          onChange={(event) => setColor(event.target.value)}
-        >
-          <option value="red">red</option>
-          <option value="green">green</option>
-          <option value="blue">blue</option>
-          <option value="yellow">yellow</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={like}
-            onChange={(event) => setLike(event.target.checked)}
-          />
-          I like colors.
-        </label>
-      </div>
-      <div>{status}</div>
-      <button onClick={saveOptions}>Save</button>
-    </React.Fragment>
+    <Container>
+      <TextField id = "api-key-input" label="API Key" value={openAiConfig.value.apiKey} onChange={onApiKeyChange}/>
+      <TextField id = "organization-id-input" label="Organization ID (optional)" value={openAiConfig.value.organizationId} onChange={onOrganizationIdChange}/>
+
+      <Button variant="contained" onClick={onSave}>Save</Button>
+    </Container>
   );
 };
 
